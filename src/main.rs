655585626db30daff13cb2f208752d868b62bc32
ItsMohi <mohi222@pm.me>
2023-03-08 17:36:59 +0330

    Add map and player

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,40 @@
-use bracket_lib::prelude::{BError, BTerm, BTermBuilder, GameState, main_loop};
+mod map;
+mod player;
 
+mod prelude {
+    pub use bracket_lib::prelude::*;
+    pub const SCREEN_WIDTH: i32 = 80;
+    pub const SCREEN_HEIGHT: i32 = 50;
+    pub use crate::map::*;
+    pub use crate::player::*;
+}
 
-struct State{}
+use prelude::*;
+
+struct State {
+    map: Map,
+    player: Player
+}
+
+impl State {
+    fn new() -> Self {
+        Self {
+            map: Map::new(),
+            player: Player::new(Point::new(10,10))
+        }
+    }
+}
 
 impl GameState for State {
     fn tick(&mut self, ctx: &mut BTerm) {
         ctx.cls();
-        ctx.print(1,1,"num num");
+        self.player.update(ctx, &self.map);
+        self.map.render(ctx);
+        self.player.render(ctx);
     }
 }
+
 fn main() -> BError {
-    let context = BTermBuilder::simple80x50().build()?;
-    main_loop(context, State {})
+    let context = BTermBuilder::simple80x50().with_fps_cap(30.0).build()?;
+    main_loop(context, State::new())
 }
---
+++ src/map.rs
@@ -0,0 +1,72 @@
+use crate::prelude::*;
+use std::ops::{Index, IndexMut};
+
+const TILES_COUNT: usize = (SCREEN_WIDTH * SCREEN_HEIGHT) as usize;
+
+#[derive(Copy, Clone, PartialEq)]
+pub enum TileType {
+    Wall,
+    Floor,
+}
+
+pub struct Map {
+    pub tiles: Vec<TileType>,
+    pub width: i32,
+    pub height: i32,
+}
+
+pub fn create_bounded_tiles(width: i32, height: i32) -> Vec<TileType> {
+    let total_tiles = (width * height) as usize;
+    let mut tiles = vec![TileType::Floor; total_tiles];
+
+    for x in 0..width {
+        tiles[x as usize] = TileType::Wall;
+        tiles[total_tiles - (x as usize) - 1] = TileType::Wall;
+    }
+
+    for y in 0..height {
+        let index = y * width;
+        tiles[index as usize] = TileType::Wall;
+        tiles[total_tiles - (index as usize) - 1] = TileType::Wall;
+    }
+
+    tiles
+}
+
+impl Map {
+    pub fn new() -> Self {
+        Self {
+            tiles: create_bounded_tiles(SCREEN_WIDTH, SCREEN_HEIGHT),
+            width: SCREEN_WIDTH,
+            height: SCREEN_HEIGHT,
+        }
+    }
+
+    pub fn tile_index(&self, point: Point) -> usize {
+        ((point.y * self.width) + point.x) as usize
+    }
+
+    pub fn in_bounds(&self, point: Point) -> bool {
+        point.x >= 0 && point.y >= 0 && point.x < self.width && point.y < self.height
+    }
+
+    pub fn can_enter_tile(&self, point: Point) -> bool {
+        self.in_bounds(point) && self.tiles[self.tile_index(point)] == TileType::Floor
+    }
+
+    pub fn render(&self, ctx: &mut BTerm) {
+        for y in 0..SCREEN_HEIGHT {
+            for x in 0..SCREEN_WIDTH {
+                let index = self.tile_index(Point::new(x, y));
+                match self.tiles[index] {
+                    TileType::Floor => {
+                        ctx.set(x, y, GRAY, BLACK, to_cp437('.'));
+                    }
+                    TileType::Wall => {
+                        ctx.set(x, y, YELLOW, BLACK, to_cp437('#'));
+                    }
+                }
+            }
+        }
+    }
+}
